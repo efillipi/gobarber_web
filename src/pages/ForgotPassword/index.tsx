330597fc1ee44d8e1641c8404d6528739e4fe2a3
Edney Fillipi <61793102+efillipi@users.noreply.github.com>
2021-10-06 02:09:42 -0300

    Esqueci minha senha

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -97,7 +97,7 @@ const ForgotPassword: React.FC = () => {
             </Button>
           </Form>
 
-          <Link to="/signin">
+          <Link to="/">
             <FiLogIn />
             Voltar ao login
           </Link>
